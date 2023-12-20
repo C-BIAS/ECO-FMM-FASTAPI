a85db4995d6b0aa4bc21ddce67155de4fbf4dd78
carlosjperez <carlos.perez@nazcamedia.net>
2023-12-20 12:47:21 +0000

    Expanded functionality and endponts for main.py
    Now with task prioritization and short - medium - long time frame categorization

--- main.py
+++ main.py
@@ -1,56 +1,53 @@
 # Import necessary modules and classes
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import List
 import sqlite3
 
-# Create FastAPI app instance
+# Create an instance of the FastAPI
 app = FastAPI()
 
-# Define Pydantic models for data validation
-
-# A Task model to create or update tasks with title, description, due date, status, priority, and dependencies
+# Define Pydantic models for data validation and structure
+# Task model for tasks API endpoint
 class Task(BaseModel):
     title: str
     description: str
-    dueDate: str
+    due_date: str
     status: str
-    # Field with validation to ensure the priority is between 1 (highest) and 5 (lowest)
-    priority: int = Field(ge=1, le=5, description="Task priority from 1 (highest) to 5 (lowest)") 
+    priority: int = Field(ge=1, le=5, description="Task priority from 1 (highest) to 5 (lowest)")
     dependencies: List[str] = []
 
-# A ChatHistory model for storing chat summaries
+# ChatHistory model for chat history API endpoint (not used in current code)
 class ChatHistory(BaseModel):
     summary: str
 
-# A UserFeedback model to store user feedback with the user ID and the feedback content
+# UserFeedback model for feedback API endpoint
 class UserFeedback(BaseModel):
     user_id: int
     feedback: str
 
 # Database utility functions
-
-# Function to establish a SQLite database connection
+# Function to connect to the database
 def connect_db():
     return sqlite3.connect('memory.sqlite')
 
-# Function to initialize the necessary tables in the SQLite database if they do not exist
+# Function to initialize the database with tables if they don't exist
 def initialize_db():
     conn = connect_db()
     cursor = conn.cursor()
-    # Table for tasks
+    # Create table for tasks
     cursor.execute('''CREATE TABLE IF NOT EXISTS Tasks (
                       id INTEGER PRIMARY KEY AUTOINCREMENT,
                       title TEXT,
                       description TEXT,
-                      dueDate TEXT,
+                      due_date TEXT,
                       status TEXT,
                       dependencies TEXT)''')
-    # Table for chat history
+    # Create table for chat history - not used but defined for future feature growth
     cursor.execute('''CREATE TABLE IF NOT EXISTS ChatHistory (
                       id INTEGER PRIMARY KEY AUTOINCREMENT,
                       summary TEXT)''')
-    # Table for user feedback
+    # Create table for feedback
     cursor.execute('''CREATE TABLE IF NOT EXISTS Feedback (
                       id INTEGER PRIMARY KEY AUTOINCREMENT,
                       user_id INT,
@@ -58,98 +55,71 @@ def initialize_db():
     conn.commit()
     conn.close()
 
-# Initialize the database as soon as the app starts
+# Initialize the database (execute the above function)
 initialize_db()
 
-# Function to check if a task exists based on its title
+# Function to check if a task already exists in the database
 def task_exists(title) -> bool:
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute("SELECT id FROM Tasks WHERE title = ?", (title,))
     task = cursor.fetchone()
     conn.close()
-    return task
+    return task is not None
 
-# FastAPI route handlers
-
-# Endpoint to retrieve all tasks
+# API route to get all tasks from the database
 @app.get("/tasks")
 def get_tasks():
-    try:
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Tasks")
-        tasks = [{"id": row[0], "title": row[1], "description": row[2], "dueDate": row[3], "status": row[4], "dependencies": row[5]} for row in cursor.fetchall()]
-        conn.close()
-        return tasks
-    except sqlite3.Error as e:
-        raise HTTPException(status_code=500, detail=f"Database error: {e}")
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Tasks")
+    tasks = [{
+        "id": row[0], "title": row[1], "description": row[2],
+        "due_date": row[3], "status": row[4],
+        "dependencies": row[5].split(",") if row[5] else []
+    } for row in cursor.fetchall()]  # Split dependencies by comma and convert to list
+    conn.close()
+    return tasks
 
-# Endpoint to create a new task or update an existing one based on its title
+# API route to create or update a task
 @app.post("/tasks", status_code=201)
 def manage_task(task: Task):
-    try:
-        conn = connect_db()
-        cursor = conn.cursor()
+    conn = connect_db()
+    cursor = conn.cursor()
+    existing_task = task_exists(task.title)  # Check if task already exists
+    if existing_task:
+        # Update existing task
+        cursor.execute("UPDATE Tasks SET description = ?, due_date = ?, status = ?, dependencies = ? WHERE title = ?",
+                       (task.description, task.due_date, task.status, ','.join(task.dependencies), task.title))
+    else:
+        # Insert new task
+        cursor.execute("INSERT INTO Tasks (title, description, due_date, status, dependencies) VALUES (?, ?, ?, ?, ?)",
+                       (task.title, task.description, task.due_date, task.status, ','.join(task.dependencies)))
+    conn.commit()
+    task_id = cursor.lastrowid  # Get the ID of the created or updated task
+    conn.close()
+    return {"task_id": task_id, "message": "Task created or updated successfully."}
 
-        # Check if a task with the provided title exists
-        existing_task = task_exists(task.title)
-        if existing_task:
-            # If task exists, update the existing task
-            cursor.execute("UPDATE Tasks SET description = ?, dueDate = ?, status = ?, dependencies = ? WHERE title = ?",
-                           (task.description, task.dueDate, task.status, ','.join(task.dependencies), task.title))
-            task_id = existing_task[0]
-        else:
-            # If task does not exist, insert a new task
-            cursor.execute("INSERT INTO Tasks (title, description, dueDate, status, dependencies) VALUES (?, ?, ?, ?, ?)",
-                           (task.title, task.description, task.dueDate, task.status, ','.join(task.dependencies)))
-            task_id = cursor.lastrowid
-
-        conn.commit()
-        return {"task_id": task_id, "message": "Task created or updated successfully."}
-    except sqlite3.Error as e:
-        raise HTTPException(status_code=500, detail=f"Database error: {e}")
-    finally:
-        # Ensure the database connection is closed
-        if conn:
-            conn.close()
+# API route to submit feedback to the database
 @app.post("/feedback", status_code=201)
 def submit_feedback(feedback: UserFeedback):
-    try:
-        # Open a new connection to the database
-        conn = connect_db()
-        cursor = conn.cursor()
-        
-        # Insert the feedback into the database
-        cursor.execute("INSERT INTO Feedback (user_id, feedback) VALUES (?, ?)",
-                       (feedback.user_id, feedback.feedback))
-        
-        # Get the ID of the newly inserted feedback
-        feedback_id = cursor.lastrowid
-        
-        # Commit the changes to the database
-        conn.commit()
-        
-        # Return a success message including the feedback ID
-        return {"feedback_id": feedback_id, "message": "Feedback submitted successfully."}
-    except sqlite3.Error as e:
-        # If there is a database error, raise an HTTPException with the detail
-        raise HTTPException(status_code=500, detail=f"Database error: {e}")
-    finally:
-        # Ensure the database connection is closed whether or not an error occurred
-        if conn:
-            conn.close()
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Feedback (user_id, feedback) VALUES (?, ?)",
+                   (feedback.user_id, feedback.feedback))
+    conn.commit()
+    feedback_id = cursor.lastrowid  # Get the ID of the submitted feedback
+    conn.close()
+    return {"feedback_id": feedback_id, "message": "Feedback submitted successfully."}
 
-# Other endpoints would be similar to the ones above and can be commented
-# accordingly when their logic is defined.
-
-# Root endpoint to display a welcome message
+# Default root API route
 @app.get("/")
 def read_root():
+    # Simple welcome message
     return {"message": "Welcome to the ECO-FMM-FASTAPI v1.1"}
 
-# Main function which runs only if this script is executed as the main program
+# Main entry point for running the app
 if __name__ == "__main__":
-    # Using Uvicorn to serve the application
     import uvicorn
+    # Start the server on host 0.0.0.0 and port 80, reloading is off for production
     uvicorn.run(app, host="0.0.0.0", port=80, reload=False)
