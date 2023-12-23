30a0854f5dc3b6fd0d18a461fcbb94172afe142a
carlosjperez <carlos.perez@nazcamedia.net>
2023-12-23 14:09:11 +0000

    commit to v2.2.0

--- replit.nix
+++ replit.nix
@@ -1,7 +1,4 @@
-{ pkgs }:
-{
-  deps = [
-    pkgs.nix-update
+nix-update
     pkgs.nix
     pkgs.mailutils
     pkgs.gitFull
@@ -9,6 +6,18 @@
     pkgs.libxcrypt
   ];
 
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.nix-update
+      pkgs.nix{pkgs}: {
+  deps = [
+    pkgs.mailutils
+    pkgs.nix-update
+    pkgs.nix
+    pkgs.gitFull
+    pkgs.imagemagick
+    pkgs.libxcrypt
+  ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.nix-update
