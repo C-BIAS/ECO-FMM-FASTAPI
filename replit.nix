1f58a749e47f5f63a09be60720fbacc3c643a2f1
carlosjperez <carlos.perez@nazcamedia.net>
2023-12-23 12:15:07 +0000

    minor corrctions on .nix

--- replit.nix
+++ replit.nix
@@ -1,16 +1,14 @@
-{pkgs}: {
+{ pkgs }:
+{
   deps = [
     pkgs.mailutils
-    pkgs.nix-update
-    pkgs.nix
     pkgs.gitFull
     pkgs.imagemagick
     pkgs.libxcrypt
   ];
+
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.nix-update
-      pkgs.nix
       pkgs.gitFull
       pkgs.libxcrypt
     ];
